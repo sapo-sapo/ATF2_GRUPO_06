2dec520c24e38fe175fccbb214e4fd4e55799aaa
sapo-sapo <cuentaparacheats32018@gmail.com>
2025-06-14 14:17:10 -0500

    14/06/25-14:17

--- src/main/java/com/buenaroma/buenaroma/repositories/ReservaRepository.java
+++ src/main/java/com/buenaroma/buenaroma/repositories/ReservaRepository.java
@@ -1,9 +1,7 @@
 package com.buenaroma.buenaroma.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.buenaroma.buenaroma.entities.Reserva;
 
-public interface ReservaRepository extends JpaRepository<ReservaRepository, Integer> {
+public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
 }
